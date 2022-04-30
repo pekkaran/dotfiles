a2021d7657f19422a9cd4d7287ea4661126cd843
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2022-04-30 13:58:02 +0300

    Add setup_dotfiles.sh.

---
+++ setup_dotfiles.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ $(pwd) != "$HOME" ]; then
+  echo "Run from home directory."
+  exit
+fi
+
+mkdir -p .config/dunst
+mkdir -p .config/mpd
+mkdir -p .config/mpv
+mkdir -p .config/nvim
+mkdir -p .config/zathura
+
+config_files=(
+  dunst/dunstrc
+  mpd/mpd.conf
+  mpv/scripts
+  mpv/input.conf
+  mpv/mpv.conf
+  nvim/init.vim
+  zathura/zathurarc
+)
+
+for file in ${config_files[@]}; do
+  ln -s "$HOME"/dotfiles/.config/"$file" .config/"$file"
+done
+
+files=(
+  .bashrc
+  .git_template
+  .tmux
+  .vim
+  .xkb
+  .alacritty.yml
+  .editorconfig
+  .i3status.conf
+  .lldbinit
+  .nethackrc
+  .tmux.conf
+  .vimrc
+  .zlogin
+  .zlogout
+  .zpreztorc
+  .zprofile
+  .zshenv
+  .zshrc
+)
+# Not included: .Xresources .xinitrc .i3
+
+for file in ${files[@]}; do
+  if [ -f "$file" ]; then
+    mv "$file" "$file.old"
+  fi
+  ln -s dotfiles/"$file" .
+done
+
+secret_files=(
+  .gitconfig
+)
+
+for file in ${secret_files[@]}; do
+  if [ -f "$file" ]; then
+    mv "$file" "$file.old"
+  fi
+  ln -s secrets/"$file" .
+done
